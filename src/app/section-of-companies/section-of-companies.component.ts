0c960674f68ffaa8e62e1b324912b613400111f9
Masha <you@example.com>
2020-06-09 11:32:13 +0300

    change header and create component with section of companies

---
+++ src/app/section-of-companies/section-of-companies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-of-companies',
+  templateUrl: './section-of-companies.component.html',
+  styleUrls: ['./section-of-companies.component.scss']
+})
+export class SectionOfCompaniesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
